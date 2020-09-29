d6d80ae1a622d7aa8b3851fc932921d6b02fac3d
Ying <sheep8844@gmail.com>
2020-09-29 14:33:55 +0800

    update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,7 +10,7 @@
 				<!-- 手機導覽列 -->
 				<div class="nav_mob">
 					<router-link to="/" class="nav-title">首頁 | 產品列表</router-link>
-					<!-- <router-link to="/admin" class="nav-title">後台管理</router-link> -->
+					<router-link to="/admin" class="nav-title">後台管理</router-link>
 				</div>
 			</div>
 
@@ -18,7 +18,7 @@
 				<!-- 電腦導覽列 -->
 				<div class="nav_pc">
 					<router-link to="/" class="nav-title">首頁 | 產品列表</router-link>
-					<!-- <router-link to="/admin" class="nav-title">後台管理</router-link> -->
+					<router-link to="/admin" class="nav-title">後台管理</router-link>
 				</div>
 
 				<!-- 購物車按鈕 -->
